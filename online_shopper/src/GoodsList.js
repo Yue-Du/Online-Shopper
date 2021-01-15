a5840141e9299e5b00d132e25efea1ab69f0ec63
Yue Du <yuedu@Yues-MacBook-Air.local>
2021-01-14 23:48:40 -0600

    front end ShowGoods

--- online_shopper/src/GoodsList.js
+++ online_shopper/src/GoodsList.js
@@ -1,24 +1,24 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Goods from './Goods'
 import tablepic from './bed table.png'
 
 class GoodsList extends Component {
-  render() {
-    const goods = [
-      {img: tablepic, goodname: 'Laptop bed tray table', price: '50$'},
-      {img: tablepic, goodname: 'desk table', price: '50$'},
-    ]
-
-    return (
-      <div >
-        <table>
-          <tr>
-            {goods.map((good, i) => <Goods good={good} key={i} />)}
-          </tr>
-        </table>
-      </div>
-    )
-  }
+    render() {
+        const goods = [
+            [{img: tablepic, goodname: 'Laptop bed tray table', price: '50$'},
+                {img: tablepic, goodname: 'desk table', price: '50$'},
+                {img: tablepic, goodname: 'desk table', price: '50$'},
+                {img: tablepic, goodname: 'desk table', price: '50$'}],
+            [{img: tablepic, goodname: 'desk table', price: '50$'}]
+        ]
+        return (<div>
+            <table>
+                {goods.map((raw, i) => <tr>{raw.map((column, j) => <td>
+                    <Goods good={column} key={j}/>
+                </td>)}</tr>)}
+            </table>
+        </div>)
+    }
 }
 
 export default GoodsList
